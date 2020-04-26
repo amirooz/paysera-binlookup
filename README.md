55e93ff4f446bf66b175c2e48e76b5f8e44a765a
MD. Amir Hossain <amirhossain0606@gmail.com>
2020-04-26 14:41:15 +0600

    Unit testing

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# paysera-binlookup
+# Paysera Binlookup
 Paysera binlookup based on bin api key
+
+<ul>
+    <li>Refactor the code</li>
+    <li>Unit testing</li>
+</ul>
+
+## Installation and use
+
+```
+$ git clone https://github.com/amirphp7/paysera-binlookup.git
+```
+```
+$ cd paysera-binlookup\app
+```
+```
+$ php app.php input.txt
+```
+```
+$ Unit Test
+```
+```
+$ composer install
+```
+**If missing any packages**
+```
+$ composer update
+```
+
+## License
+Paysera Binlookup is open-sourced software licensed under the [GPL-3.0 license](https://opensource.org/licenses/GPL-3.0).
+Frameworks and libraries has it own licensed
+
+
--- tests/AppTest.php
+++ tests/AppTest.php
@@ -10,12 +10,26 @@ class AppTest extends TestCase
     
     public function testBinLookup()
     {
+        $app = new \App\Task();
+
+        $app->binLookup(41417360);
+
+        $this->assertEquals($app->bin->country->alpha2, 'US');
+
+    }
+
+    //check the result in two different way
+    public function testCheckResult()
+    {
+        $binKey = 45417360;
+
+        $app = new \App\Task();
+        $app->binLookup($binKey);
+
         $bin = PhpBIN::getInstance('BinList');
+        $info = $bin->getInfo($binKey);
 
-        $info = $bin->getInfo("45417360");
-
-        $this->assertEquals($info['CC_ISO3166_1'], 'JP');
-
+        $this->assertEquals($app->bin->country->alpha2, $info['CC_ISO3166_1']);
     }
 
 
